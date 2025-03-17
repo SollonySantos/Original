0fbc8a6d1402bdf2a34df97c7cc780bd52e0dd1d
SollonySantos <sollonysantos8@gmail.com>
2025-03-17 15:34:53 -0300

    Removi a pasta Original do stage e adicionei ao .gitignore

--- backend/mercadopago-config.js
+++
@@ -1,9 +0,0 @@
-// mercadopago-config.js
-const mercadopago = require('mercadopago');
-
-// Configuração do Mercado Pago
-mercadopago.configure({
-    access_token: 'APP_USR-2406878334819464-030321-e4ce3a324579f445da870bba47cd6008-1306813266', // Substitua pelo seu Access Token do Mercado Pago
-});
-
-module.exports = mercadopago;
--- test-mercadopago.js
+++
@@ -1,7 +0,0 @@
-const mercadopago = require('mercadopago');
-
-mercadopago.configure({
-    access_token: process.env.ACCESS_TOKEN, // Usa o token do .env
-});
-
-console.log('Mercado Pago configurado com sucesso!');
